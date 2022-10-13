8cd67b0b32037f1faf468dc274d6ba01dbbc52cb
Haylee Anderson <hayleealyssalyn.anderson@finastra.com>
2022-10-13 12:12:14 -0400

    changed style value

--- amaltheafs-cerm_advanced-f2c913bc33b2/main/greenStreamlit.py
+++ amaltheafs-cerm_advanced-f2c913bc33b2/main/greenStreamlit.py
@@ -84,7 +84,7 @@ def sideBar():
     horizon = st.sidebar.number_input( 'Horizon', 1, 50,value=10)
     st.sidebar.markdown( """ 
                             <p style= font-size:12px;color:#898A8B;margin-top:-86px;margin-left:50px;'>
-                            -- time horizon of the study
+                            - time horizon of the study
                             </p>
                         """, unsafe_allow_html=True
                         )
@@ -92,57 +92,55 @@ def sideBar():
     alpha = st.sidebar.slider( 'Alpha ', min_value=0.001, step=0.001, max_value=0.5, value=.02, format="%f")
     st.sidebar.markdown( """ 
                             <p style= font-size:12px;color:#898A8B;margin-top:-100px;margin-left:40px;'>
-                            -- transition efficiency coefficient (reduced)
+                            - transition efficiency coefficient (reduced)
                             </p>
                         """, unsafe_allow_html=True
                         )
     #transition effort reactivity coefficient
     beta = st.sidebar.slider( 'Beta', min_value=0.001, step=0.001, max_value=5.0, value=1.5, format="%f")
     st.sidebar.markdown( """ 
-                            <p style= font-size:13px;color:#898A8B;margin-top:-101px;margin-left:35px;'>
-                            -- transition effort reactivity coefficient
+                            <p style= font-size:12px;color:#898A8B;margin-top:-101px;margin-left:35px;'>
+                            - transition effort reactivity coefficient
                             </p>
                         """, unsafe_allow_html=True
                         )
     #climate change intensity of the economic activity (idiosyncratic)
-    #st.sidebar.slider( label="test", min_value=0.000, step=0.001, max_value=0.005, value=0.045, format="%f", )
-
     gamma = st.sidebar.slider( 'Gamma', min_value=0.001, step=0.001, max_value=1.0, value=0.005, format="%f", )
     st.sidebar.markdown( """ 
                         <p style= font-size:11px;color:#898A8B;margin-top:-99px;margin-left:50px;'>
-                        -- climate change intensity of economic activity
+                        - climate change intensity of economic activity
                         </p>
                     """, unsafe_allow_html=True
                     )
     #hypothetical climate-free average growth rate of log GDP
     R = st.sidebar.slider( 'R', 1.00, 3.00, value=1.00)
     st.sidebar.markdown( """ 
-                        <p style= font-size:13px;color:#898A8B;margin-top:-101px;margin-left:20px;'>
-                        -- climate-free average growth rate of log GDP
+                        <p style= font-size:12px;color:#898A8B;margin-top:-101px;margin-left:20px;'>
+                        - climate-free average growth rate of log GDP
                         </p>
                     """, unsafe_allow_html=True
                     )
     #idiosyncratic economic risk    
     e = st.sidebar.slider( 'e', min_value=0.001, step=0.001, max_value=1.0, value=0.10)
     st.sidebar.markdown( """ 
-                    <p style= font-size:13px;color:#898A8B;margin-top:-100px;margin-left:20px;'>
-                    -- idiosyncratic economic risk
+                    <p style= font-size:12px;color:#898A8B;margin-top:-100px;margin-left:20px;'>
+                    - idiosyncratic economic risk
                     </p>
                 """, unsafe_allow_html=True
                 )
     #idiosyncratic physical risk
     p = st.sidebar.slider( 'p', min_value=0.001,step=0.001, max_value=1.0,value=0.10)
     st.sidebar.markdown( """ 
-                <p style= font-size:13px;color:#898A8B;margin-top:-100px;margin-left:20px;'>
-                -- idiosyncratic physical risk
+                <p style= font-size:12px;color:#898A8B;margin-top:-100px;margin-left:20px;'>
+                -2 idiosyncratic physical risk
                 </p>
             """, unsafe_allow_html=True
             )
     #independent transition coefficient
     theta = st.sidebar.slider( 'theta', min_value=0.001, step=0.001, max_value=0.3,value=0.10)
     st.sidebar.markdown( """ 
-            <p style= font-size:13px;color:#898A8B;margin-top:-100px;margin-left:40px;'>
-            -- independent transition coefficient
+            <p style= font-size:12px;color:#898A8B;margin-top:-100px;margin-left:40px;'>
+            - independent transition coefficient
             </p>
         """, unsafe_allow_html=True
         )
@@ -151,7 +149,7 @@ def sideBar():
     N = st.sidebar.number_input('N', 1, value=10)  
     st.sidebar.markdown( """ 
             <p style= font-size:12px;color:#898A8B;margin-top:-87px;margin-left:20px;'>
-            -- number of iterations for Monte-Carlo simulaton
+            - number of iterations for Monte-Carlo simulaton
             </p>
         """, unsafe_allow_html=True
         )
